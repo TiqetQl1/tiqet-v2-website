8c51beb158760cbd7314ba49106a8a6b72f49ade
Kian Arabi <k80arabi@outlook.com>
2025-05-23 18:50:45 +0330

    More boilerplace

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
+import * as path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tsconfigPaths(),svgr()],
+  resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
+      ],
+  },
 })
