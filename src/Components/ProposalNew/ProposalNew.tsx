01c905a3050b21d299d8d933e7182002d93399be
Kian Arabi <k80arabi@outlook.com>
2025-05-30 21:21:38 +0330

    Implements parts of proposals front-end

--- src/Components/ProposalNew/ProposalNew.tsx
+++ src/Components/ProposalNew/ProposalNew.tsx
@@ -1,7 +1,62 @@
+/// <reference types="vite-plugin-svgr/client" />
+import SendSvg from "@/assets/material-theme/icons/singles/send.svg?react";
+
+import { useEffect, useRef, useState } from "react"
+import styles from "./ProposalNew.module.scss"
+
+import * as placeHolders from "@/utils/defaultsToCreateProposal.json";
+import Progress from "../Shared/Progress/Progress";
+
 const ProposalNew = () => {
-    return <div>
-        ProposalNew
-    </div>
+    const [isSending, setIsSending] = useState<boolean>(false)
+    const titleRef = useRef<HTMLInputElement>(document.createElement("input"))
+    const textRef = useRef<HTMLTextAreaElement>(document.createElement("textarea"))
+
+    const resetInputs = () => {
+        titleRef.current.value  = ""
+        textRef.current.value   = ""
+    }
+
+    const sendProposal = async () => {
+        await setIsSending(prev=>!prev)
+
+    }
+
+    useEffect(resetInputs,[titleRef, textRef])
+
+    return <main className={styles.p_new}>
+        <header>
+            <h2>
+                Propose
+            </h2>
+            <button disabled={isSending} onClick={sendProposal}>
+                {isSending ? <Progress /> : <SendSvg />}
+                <span>
+                    {isSending ? "Please wait" : "Send"}
+                </span>
+            </button>
+        </header>
+        <section>
+            <input 
+                type="text" 
+                name="title" 
+                id="new-proposal-title" 
+                ref={titleRef} 
+                placeholder={placeHolders.title}/>
+            <textarea 
+                name="text" 
+                id="new-proposal-text" 
+                ref={textRef}
+                placeholder={placeHolders.text}
+                onInput={(e)=>{
+                    const ele = e.currentTarget;
+                    ele.style.height 
+                        = ele.scrollHeight + "px";
+                }}
+                >
+            </textarea>
+        </section>
+    </main>
 }
 
 export default ProposalNew
--- src/Components/Proposals/Proposals.tsx
+++ src/Components/Proposals/Proposals.tsx
@@ -1,7 +1,27 @@
+/// <reference types="vite-plugin-svgr/client" />
+import EditSvg from "@/assets/material-theme/icons/singles/edit.svg?react";
+
+import { Link } from "react-router"
+import styles from "./Proposals.module.scss"
+
 const Proposals = () => {
-    return <div>
-        Proposals
-    </div>
+    return <main className={styles.proposals}>
+        <section>
+            <h2>
+                Event proposals
+            </h2>
+            <ul>
+                <li>
+                    hi
+                </li>
+            </ul>
+        </section>
+        <button className={styles.FAB}>
+            <Link to="./new">
+                <EditSvg/>
+            </Link>
+        </button>
+    </main>
 }
 
 export default Proposals
