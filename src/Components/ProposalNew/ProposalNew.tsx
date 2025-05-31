a097480938938b5ac333fff062e4a310129a8822
Kian Arabi <k80arabi@outlook.com>
2025-05-31 17:39:25 +0330

    Implements newProposal page

--- src/Components/ProposalNew/ProposalNew.tsx
+++ src/Components/ProposalNew/ProposalNew.tsx
@@ -6,11 +6,43 @@ import styles from "./ProposalNew.module.scss"
 
 import * as placeHolders from "@/utils/defaultsToCreateProposal.json";
 import Progress from "../Shared/Progress/Progress";
+import { simulateContract, writeContract } from "@wagmi/core";
+import { wagmiConfig } from "@/utils/wagmiConfig";
+import { coreAbi, coreAddress } from "@/utils/Contracts/core";
+import useERC20 from "@/hooks/useERC20";
+import { qusdtAddress } from "@/utils/Contracts/qusdt";
+import { useAccount, useReadContract } from "wagmi";
+import { activeChain } from "@/utils/Contracts/activeChain";
+import { bigIntToFixed } from "@/utils";
 
 const ProposalNew = () => {
-    const [isSending, setIsSending] = useState<boolean>(false)
+    const { address: user } = useAccount()
+    const [isSending, setIsSending] = useState<boolean>(true)
     const titleRef = useRef<HTMLInputElement>(document.createElement("input"))
     const textRef = useRef<HTMLTextAreaElement>(document.createElement("textarea"))
+    const [loadingText, setLoadingText] = useState<string>("Getting fee")
+
+    const { data: fee } = useReadContract({
+        abi: coreAbi,
+        chainId: activeChain.id,
+        config: wagmiConfig,
+        address: coreAddress,
+        functionName: "_proposal_fee"
+    })
+    const qusdt = useERC20(qusdtAddress)
+
+    const realFee = () => {
+        if (fee == undefined || qusdt.decimals==undefined) {
+            return undefined
+        }
+        return bigIntToFixed(fee, qusdt.decimals)
+    }
+
+    useEffect(()=>{
+        if (fee == undefined && qusdt.decimals==undefined) {
+            setIsSending(false)
+        }
+    },[fee, qusdt.decimals])
 
     const resetInputs = () => {
         titleRef.current.value  = ""
@@ -18,8 +50,38 @@ const ProposalNew = () => {
     }
 
     const sendProposal = async () => {
-        await setIsSending(prev=>!prev)
-
+        await setIsSending(true)
+        await setLoadingText("Please wait")
+        try {
+            await setLoadingText("Requesting allowance")
+            if(fee==undefined){
+                await setLoadingText("fee is not fetched")
+                setTimeout(()=>setIsSending(false), 3000)
+                return
+            }
+            await qusdt.approve(coreAddress, BigInt(fee))
+            await setLoadingText("Sending Proposal")
+            // console.log("simulating")
+            const { request } = await simulateContract(wagmiConfig,{
+                address: coreAddress,
+                abi: coreAbi,
+                functionName: "eventPropose",
+                args: [
+                    JSON.stringify({
+                        title: titleRef.current.value,
+                        text : textRef.current.value
+                    })
+                ]
+            })
+            // console.log("sending")
+            const _hash = await writeContract(wagmiConfig, request)
+        } catch (_err) {
+            await setLoadingText("Failed")
+            setTimeout(()=>setIsSending(false), 3000)
+            return
+        }
+        await setLoadingText("Sent")
+        setTimeout(()=>setIsSending(false), 3000)
     }
 
     useEffect(resetInputs,[titleRef, textRef])
@@ -32,7 +94,11 @@ const ProposalNew = () => {
             <button disabled={isSending} onClick={sendProposal}>
                 {isSending ? <Progress /> : <SendSvg />}
                 <span>
-                    {isSending ? "Please wait" : "Send"}
+                    {
+                        isSending 
+                            ? loadingText 
+                            : `Send (${realFee()==undefined?'':realFee()} ${qusdt.symbol} fee )`
+                    }
                 </span>
             </button>
         </header>
